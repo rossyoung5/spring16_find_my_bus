56f5cd3fed0afcf447a3300608856d7a4768c4c1
Ross Young <ross.young12@gmail.com>
2016-05-12 10:35:58 -0500

    adds method to geolocate user address input

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,9 @@
 class Location < ActiveRecord::Base
+geocoded_by :full_street_address   # can also be an IP address
+after_validation :geocode          # auto-fetch coordinates
+
+  def full_street_address
+    "#{street_address}, #{city}, GA"
+  end
+
 end
